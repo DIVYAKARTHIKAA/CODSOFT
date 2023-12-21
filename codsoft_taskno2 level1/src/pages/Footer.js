7194381e3877679f8c3b7b685bf0373847f4d8a3
unknown <727821tucs046@skct.edu.in>
2023-12-21 23:31:42 +0530

    added

---
+++ codsoft_taskno2 level1/src/pages/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+    <footer>
+    <p>Divya Karthikaa</p>
+    
+    <p>Check out my coding projects on GitHub <br></br>
+    Connect with me on LinkedIn,Dribble for updates and more.</p>
+
+    <div class="social">
+    <a href="https://github.com/DIVYAKARTHIKAA"><i class="fab fa-github"></i></a>
+    <a href="https://www.linkedin.com/in/divyakarthikaa-jp-105b11228/"><i class="fab fa-linkedin"></i></a>
+    <a href="https://dribbble.com/Divyakarthikaa"><i class="fab fa-dribbble"></i></a>
+    </div>
+    <p class="end">Copyright By DivyaKarthikaa</p>
+</footer>
+</div>
+  )
+}
+
+export default Footer
